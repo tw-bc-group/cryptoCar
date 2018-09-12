a2e39f589ab8c6687fea67e8593e7ee28caad098
sqliu <sqliu@thoughtworks.com>
2018-09-12 14:53:13 +0800

    add log

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.post('/cars', (req, res) => {
 app.get('/features', (req, res) => {
     console.log('**** GET / POIs ****');
     const { boundingbox, scale, position, heading, VIN } = req.query;
+    console.log(position)
     truffle_connect.feature(boundingbox, scale, position, heading, VIN, (data) => {
         res.send(data);
     });
