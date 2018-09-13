190df8828d3a69c9f6e018f0d8a34e4c1c5d5c41
rldan <rldan@thoughtworks.com>
2018-09-14 04:38:34 +0800

    test

--- server.js
+++ server.js
@@ -50,146 +50,146 @@ app.post('/app', (req, res) => {
     }
 });
 
-app.get('/features', (req, res) => {
-    console.log('**** GET / POIs ****');
-    let { boundingbox, scale, position, heading, VIN } = req.query;
-    console.log(boundingbox, position);
-    if(!VIN){
-        VIN = '10000';
-    }
-    truffle_connect.features(boundingbox, scale, position, heading, VIN, (data) => {
-        console.log(data);
-        res.send(data);
-    });
-});
-
 // app.get('/features', (req, res) => {
 //     console.log('**** GET / POIs ****');
-//
-//     const { boundingbox, scale, position} = req.query;
-//     console.log(req.query);
-//     console.log(position);
-//     if (!position) {
-//         console.log('no possition');
-//         res.send({"features": []});
-//         return;
+//     let { boundingbox, scale, position, heading, VIN } = req.query;
+//     console.log(boundingbox, position);
+//     if(!VIN){
+//         VIN = '10000';
 //     }
-//
-//     let [lat, lont] = position.split(',').map(i => parseFloat(i));
-//
-//     // const { boundingbox, scale, position, heading, VIN } = req.query;
-//     // console.log(position);
-//     // truffle_connect.features(boundingbox, scale, position, heading, VIN, (data) => {
-//     //     res.send(data);
-//     // });
-//
-//
-//     const result =
-//         {
-//             "features": [
-//                 {
-//                     "id": "66666666",
-//                     "geometry": {
-//                         "points": [
-//                             {
-//                                 "location": [
-//                                     lat+0.001,
-//                                     lont+0.001
-//                                 ],
-//                                 "icon": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
-//                                 "isAccess": true
-//                             }
-//                         ]
-//                     },
-//                     "lastModified": (new Date()).toISOString(),
-//                     "name": "CryptoCar",
-//                     "address": {
-//                         "street": "CryptoCar"
-//                     },
-//                     "listIconSet": {
-//                         "url": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
-//                         "lastModified": (new Date()).toISOString()
-//                     },
-//                     "reference": {
-//                         "extended": {
-//                             "lastModified": (new Date()).toISOString()
-//                         }
-//                     },
-//                     "mapFeatureType": "lbs-aop",
-//                     "isDestination": true,
-//                     "isSelectable": true
-//                 },
-//                 {
-//                     "id": "77777777",
-//                     "geometry": {
-//                         "points": [
-//                             {
-//                                 "location": [
-//                                     lat+0.002,
-//                                     lont-0.001
-//                                 ],
-//                                 "icon": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
-//                                 "isAccess": true
-//                             }
-//                         ]
-//                     },
-//                     "lastModified": (new Date()).toISOString(),
-//                     "name": "CryptoCar",
-//                     "address": {
-//                         "street": "CryptoCar"
-//                     },
-//                     "listIconSet": {
-//                         "url": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
-//                         "lastModified": (new Date()).toISOString()
-//                     },
-//                     "reference": {
-//                         "extended": {
-//                             "lastModified": (new Date()).toISOString()
-//                         }
-//                     },
-//                     "mapFeatureType": "lbs-aop",
-//                     "isDestination": true,
-//                     "isSelectable": true
-//                 },
-//                 {
-//                     "id": "888888",
-//                     "geometry": {
-//                         "points": [
-//                             {
-//                                 "location": [
-//                                     //Tiananmen
-//                                     39.9087,
-//                                     116.3975
-//                                 ],
-//                                 "icon": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
-//                                 "isAccess": true
-//                             }
-//                         ]
-//                     },
-//                     "lastModified": (new Date()).toISOString(),
-//                     "name": "CryptoCar",
-//                     "address": {
-//                         "street": "CryptoCar"
-//                     },
-//                     "listIconSet": {
-//                         "url": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
-//                         "lastModified": (new Date()).toISOString()
-//                     },
-//                     "reference": {
-//                         "extended": {
-//                             "lastModified": (new Date()).toISOString()
-//                         }
-//                     },
-//                     "mapFeatureType": "lbs-aop",
-//                     "isDestination": true,
-//                     "isSelectable": true
-//                 }
-//             ]
-//         };
-//
-//     res.send(result)
-//
+//     truffle_connect.features(boundingbox, scale, position, heading, VIN, (data) => {
+//         console.log(data);
+//         res.send(data);
+//     });
 // });
+
+app.get('/features', (req, res) => {
+    console.log('**** GET / POIs ****');
+
+    const { boundingbox, scale, position} = req.query;
+    console.log(req.query);
+    console.log(position);
+    if (!position) {
+        console.log('no possition');
+        res.send({"features": []});
+        return;
+    }
+
+    let [lat, lont] = position.split(',').map(i => parseFloat(i));
+
+    // const { boundingbox, scale, position, heading, VIN } = req.query;
+    // console.log(position);
+    // truffle_connect.features(boundingbox, scale, position, heading, VIN, (data) => {
+    //     res.send(data);
+    // });
+
+
+    const result =
+        {
+            "features": [
+                {
+                    "id": "66666666",
+                    "geometry": {
+                        "points": [
+                            {
+                                "location": [
+                                    lat+0.001,
+                                    lont+0.001
+                                ],
+                                "icon": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
+                                "isAccess": true
+                            }
+                        ]
+                    },
+                    "lastModified": (new Date()).toISOString(),
+                    "name": "CryptoCar",
+                    "address": {
+                        "street": "CryptoCar"
+                    },
+                    "listIconSet": {
+                        "url": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
+                        "lastModified": (new Date()).toISOString()
+                    },
+                    "reference": {
+                        "extended": {
+                            "lastModified": (new Date()).toISOString()
+                        }
+                    },
+                    "mapFeatureType": "lbs-aop",
+                    "isDestination": true,
+                    "isSelectable": true
+                },
+                {
+                    "id": "77777777",
+                    "geometry": {
+                        "points": [
+                            {
+                                "location": [
+                                    lat+0.002,
+                                    lont-0.001
+                                ],
+                                "icon": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
+                                "isAccess": true
+                            }
+                        ]
+                    },
+                    "lastModified": (new Date()).toISOString(),
+                    "name": "CryptoCar",
+                    "address": {
+                        "street": "CryptoCar"
+                    },
+                    "listIconSet": {
+                        "url": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
+                        "lastModified": (new Date()).toISOString()
+                    },
+                    "reference": {
+                        "extended": {
+                            "lastModified": (new Date()).toISOString()
+                        }
+                    },
+                    "mapFeatureType": "lbs-aop",
+                    "isDestination": true,
+                    "isSelectable": true
+                },
+                {
+                    "id": "888888",
+                    "geometry": {
+                        "points": [
+                            {
+                                "location": [
+                                    //Tiananmen
+                                    39.9087,
+                                    116.3975
+                                ],
+                                "icon": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
+                                "isAccess": true
+                            }
+                        ]
+                    },
+                    "lastModified": (new Date()).toISOString(),
+                    "name": "CryptoCar",
+                    "address": {
+                        "street": "CryptoCar"
+                    },
+                    "listIconSet": {
+                        "url": "https://lbsapi-ntg6freshup2-test.azure.mercedes-benz.com/api/images/icons/Other/automotive.png",
+                        "lastModified": (new Date()).toISOString()
+                    },
+                    "reference": {
+                        "extended": {
+                            "lastModified": (new Date()).toISOString()
+                        }
+                    },
+                    "mapFeatureType": "lbs-aop",
+                    "isDestination": true,
+                    "isSelectable": true
+                }
+            ]
+        };
+
+    res.send(result)
+
+});
 
 app.get('/details/:id', (req, res) => {
     console.log('**** GET / POI detail ****');
