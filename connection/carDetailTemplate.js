9efc331fd9998e10b6b790b4ee220a90cb400fda
RuipengWang <>
2018-09-14 07:52:46 +0800

    revert back to original example -- Ruipeng Wang

--- connection/carDetailTemplate.js
+++ connection/carDetailTemplate.js
@@ -69,50 +69,134 @@ module.exports = {
                 }]
         }
         // return detail;
-        return (
-            {
-                "location": "北京",
-                "addPOIinfos": [
-                    {
-                        "type": "subheader",
-                        "text": "Detail"
-                    },
-                    {
-                        "type": "row",
-                        "entries": [
-                            {
-                                "type": "column",
-                                "width": 100,
-                                "entries": [
-                                    {
-                                        "type": "row",
-                                        "entries": [
-                                            {
-                                                "type": "image",
-                                                "picture": {
-                                                    "base64": null,
-                                                    "url": null,
-                                                },
-                                                "verticalAlignment": "top",
-                                                "horizontalAlignment": "left",
+        // return (
+        //     {
+        //         "location": "北京",
+        //         "addPOIinfos": [
+        //             {
+        //                 "type": "subheader",
+        //                 "text": "Detail"
+        //             },
+        //             {
+        //                 "type": "row",
+        //                 "entries": [
+        //                     {
+        //                         "type": "column",
+        //                         "width": 100,
+        //                         "entries": [
+        //                             {
+        //                                 "type": "row",
+        //                                 "entries": [
+        //                                     {
+        //                                         "type": "image",
+        //                                         "picture": {
+        //                                             "base64": null,
+        //                                             "url": null,
+        //                                         },
+        //                                         "verticalAlignment": "top",
+        //                                         "horizontalAlignment": "left",
+        //                                     },
+        //                                     {
+        //                                         "type": "text",
+        //                                         "width": 70,
+        //                                         "verticalAlignment": "center",
+        //                                         "horizontalAlignment": "left",
+        //                                         "font": "f12",
+        //                                         "color": "color_c07a",
+        //                                         "text": "test!!!!!!!!!"
+        //                                     }
+        //                                 ]
+        //                             },
+        //                         ]
+        //                     }
+        //                 ]
+        //             }
+        //         ]
+        //     }
+        // );
+        return {
+            "location": "北京",
+            "addPOIinfos": [
+                {
+                    "type": "subheader",
+                    "text": "DETAILS"
+                },
+                {
+                    "type": "row",
+                    "entries": [
+                        {
+                            "type": "column",
+                            "width": 100,
+                            "entries": [
+                                {
+                                    "type": "row",
+                                    "entries": [
+                                        {
+                                            "type": "image",
+                                            "picture": {
+                                                "base64": ""
                                             },
-                                            {
-                                                "type": "text",
-                                                "width": 70,
-                                                "verticalAlignment": "center",
-                                                "horizontalAlignment": "left",
-                                                "font": "f12",
-                                                "color": "color_c07a",
-                                                "text": "test!!!!!!!!!"
-                                            }
-                                        ]
-                                    },
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        );
+                                            "horizontalAlignment": "right",
+                                            "width": 30
+                                        },
+                                        {
+                                            "type": "text",
+                                            "width": 70,
+                                            "verticalAlignment": "center",
+                                            "horizontalAlignment": "left",
+                                            "font": "f12",
+                                            "color": "color_c07a",
+                                            "text": "(1611), 23333/Person"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "row",
+                                    "entries": [
+                                        {
+                                            "type": "image",
+                                            "picture": {
+                                                "base64": ""
+                                            },
+                                            "horizontalAlignment": "center",
+                                            "width": 10
+                                        },
+                                        {}
+                                    ]
+                                },
+                                {
+                                    "type": "row",
+                                    "width": 90,
+                                    "verticalAlignment": "center",
+                                    "horizontalAlignment": "left",
+                                    "font": "f12",
+                                    "color": "color_c07a",
+                                    "text": "周一至周日\n00:00-21:00",
+                                    "entries": [
+                                        {
+                                            "type": "image",
+                                            "picture": {
+                                                "base64": ""
+                                            },
+                                            "horizontalAlignment": "center",
+                                            "width": 10
+                                        },
+                                        {
+                                            "type": "text",
+                                            "width": 90,
+                                            "verticalAlignment": "center",
+                                            "horizontalAlignment": "left",
+                                            "font": "f12",
+                                            "color": "color_c07a",
+                                            "text": "LBS\nLBS"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
     }
 };
