93bb2287c6f190455827cf8ef7fba2c7ed4b21a4
RuipengWang <>
2018-09-13 17:09:26 +0800

    S1.Refactor app template, extract build image as method -- Ruipeng Wang

--- connection/onlineUIApp.js
+++ connection/onlineUIApp.js
@@ -1,3 +1,99 @@
+const buildImage = (url, base64, asset, uiImage) => {
+    return {
+        "url": url,
+        "base64": base64,
+        "asset": asset,
+        "uiImage": uiImage
+    };
+}
+const myCarEntry_CarouselEntry = {
+                                    "voiceMessage": "Provided by ThoughtWorks",
+                                    "image": buildImage("http://42.159.6.37:3000/images/carpic6.png", null, null, null), // Notation: This is for show the car image
+                                    "title": "My Car",
+                                    "starsImageEntryKeepSize": null, 
+                                    "starsImageDescriptionKeepSize": null, 
+                                    "starsImage": buildImage(null, null, null, null), // Notation: This is for reviewer, which means this is actually shows the star.
+                                    "reviews": null,
+                                    "subtitle1": "Car property 1",
+                                    "subtitle2": "Car property 2",
+                                    "imageHeader": "My Crypto Car",
+                                    "imageFooter": "Owner"
+                                }
+const myCarEntry_DetailEntry = {
+                                    "type": "locationBased",
+                                    "title": "My Car",
+                                    "subtitle": "Description 1", 
+                                    "headerImage": buildImage("http://42.159.6.37:3000/images/carpic6.png", null, null, null),
+                                    "starsImage": buildImage(null, null, null, null), 
+                                    "voiceMessage": "Provided by ThoughtWorks", 
+                                    // "favouritesButton": {
+                                        // "id": "favouritesButton_00", 
+                                        // "icon": {}, 
+                                        // "deletePopup": {
+                                        //     "message": "Do you want to delete this?",
+                                        //     "yesButton": {
+                                        //         "text": "Yes"
+                                        //     },
+                                        //     "noButton": {
+                                        //         "text": "No"
+                                        //     } 
+                                        // }
+                                    // },
+                                    "favouritesButton": null,
+                                    "rows": [{
+                                        // "id": "contractId", // Notation: If you just want to show the text in it, you actually don't need to set ID for it.
+                                        "type": "oneLineButton",
+                                        "image": buildImage(null, null, null, null),
+                                        "primaryText": "contract Id ... ",
+                                        "actionIcon": null
+                                    },
+                                    {
+                                        // "id": "buttonID02", // Notation: If you just want to show the text in it, you actually don't need to set ID for it.
+                                        "type": "twoLineButton",
+                                        "image": buildImage(null, null, null, null), 
+                                        "primaryText": "Primary Text", 
+                                        "secondaryText": "Secondary Text", 
+                                        "actionIcon": null, 
+                                        "useOneFont": true
+                                    },
+                                    {
+                                        "type": "featureList",
+                                        "content":[
+                                            {
+                                                "title": "Badge 1",
+                                                "icon": buildImage(null, null, "navigation/direction_arrow_01", null)
+                                            },{
+                                                "title": "Badge 2",
+                                                "icon": buildImage(null, null, "navigation/direction_arrow_01", null)
+                                            },{
+                                                "title": "Badge 3",
+                                                "icon": buildImage(null, null, "navigation/direction_arrow_01", null)
+                                            },{
+                                                "title": "Badge 4",
+                                                "icon": buildImage(null, null, "navigation/direction_arrow_01", null)
+                                            },{
+                                                "title": "Badge 5",
+                                                "icon": buildImage(null, null, "navigation/direction_arrow_01", null)
+                                            },{
+                                                "title": "Badge 6",
+                                                "icon": buildImage(null, null, "navigation/direction_arrow_01", null)
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": "pictureGallery",
+                                        "content": [
+                                            buildImage("http://42.159.6.37:3000/images/carpic6.png", null, null, null),
+                                            buildImage("http://42.159.6.37:3000/images/carpic6.png", null, null, null),
+                                            buildImage("http://42.159.6.37:3000/images/carpic6.png", null, null, null)
+                                        ]
+                                    }]
+                                }
+const myCarEntry = {
+                        "id": "carouselEntry01",
+                        "carouselEntry": myCarEntry_CarouselEntry,
+                        "detailsEntry": myCarEntry_DetailEntry
+                    }
 module.exports = {
     startApp: function () {
         return ({
@@ -5,152 +101,11 @@ module.exports = {
                 "tabs": [{
                     "title": "Crypto Car",
                     "emptyListText": "No matched car",
-                    // "providedByText": "Provided by ThoughtWorks", // Notation: If just has one tab, then the provided text will show on the title
+                    "providedByText": "Provided by ThoughtWorks", // Notation: If just has one tab, then the provided text will show on the title
                     "providedByText": "Crypto Car",
                     "providedByLogo": null,
                     "leftLogo": null,
-                    "entries": [{
-                        "id": "carouselEntry01",
-                        "carouselEntry": {
-                            "voiceMessage": "Provided by ThoughtWorks",
-                            "image": {
-                                "base64": null,
-                                "url": "http://42.159.6.37:3000/images/carpic6.png",
-                                "asset": null
-                            }, // Notation: This is for show the car image
-                            "title": "My Car",
-                            "starsImageEntryKeepSize": null, 
-                            "starsImageDescriptionKeepSize": null, 
-                            "starsImage": {
-                                "url": null,
-                                // "uiImage": "53_mercedes_me/53_restaurant_recommendation_star_icons/dianping_stars_4",
-                                "uiImage": null,
-                                "base64": null,
-                                "asset": null
-                            }, // Notation: This is for reviewer, which means this is actually shows the star.
-                            "reviews": null,
-                            "subtitle1": "Car property 1",
-                            "subtitle2": "Car property 2",
-                            "imageHeader": "My Crypto Car",
-                            "imageFooter": "Owner"
-                        },
-                        "detailsEntry": {
-                            "type": "locationBased",
-                            "title": "My Car",
-                            "subtitle": "Description 1", 
-                            "headerImage": {
-                                "base64": null,
-                                "url": "http://42.159.6.37:3000/images/carpic6.png",
-                                "asset": null
-                            },
-                            "starsImage": null, 
-                            "voiceMessage": "Provided by ThoughtWorks", 
-                            // "favouritesButton": {
-                                // "id": "favouritesButton_00", 
-                                // "icon": {}, 
-                                // "deletePopup": {
-                                //     "message": "Do you want to delete this?",
-                                //     "yesButton": {
-                                //         "text": "Yes"
-                                //     },
-                                //     "noButton": {
-                                //         "text": "No"
-                                //     } 
-                                // }
-                            // },
-                            "favouritesButton": null,
-                            "rows": [{
-                                // "id": "contractId", // Notation: If you just want to show the text in it, you actually don't need to set ID for it.
-                                "type": "oneLineButton",
-                                "image": null,
-                                "primaryText": "contract Id ... ",
-                                "actionIcon": null
-                            },
-                            {
-                                // "id": "buttonID02", // Notation: If you just want to show the text in it, you actually don't need to set ID for it.
-                                "type": "twoLineButton",
-                                "image": null, 
-                                "primaryText": "Primary Text", 
-                                "secondaryText": "Secondary Text", 
-                                "actionIcon": null, 
-                                "useOneFont": true
-                            },
-                            {
-                                "type": "featureList",
-                                "content":[
-                                    {
-                                        "title": "Badge 1",
-                                        "icon":{
-                                            "url": null,
-                                            "uiImage": null,
-                                            "base64": null,
-                                            "asset": "navigation/direction_arrow_01"
-                                        }
-                                    },{
-                                        "title": "Badge 2",
-                                        "icon":{
-                                            "url": null,
-                                            "uiImage": null,
-                                            "base64": null,
-                                            "asset": "navigation/direction_arrow_01"
-                                        }
-                                    },{
-                                        "title": "Badge 3",
-                                        "icon":{
-                                            "url": null,
-                                            "uiImage": null,
-                                            "base64": null,
-                                            "asset": "navigation/direction_arrow_01"
-                                        }
-                                    },{
-                                        "title": "Badge 4",
-                                        "icon":{
-                                            "url": null,
-                                            "uiImage": null,
-                                            "base64": null,
-                                            "asset": "navigation/direction_arrow_01"
-                                        }
-                                    },{
-                                        "title": "Badge 5",
-                                        "icon":{
-                                            "url": null,
-                                            "uiImage": null,
-                                            "base64": null,
-                                            "asset": "navigation/direction_arrow_01"
-                                        }
-                                    },{
-                                        "title": "Badge 6",
-                                        "icon":{
-                                            "url": null,
-                                            "uiImage": null,
-                                            "base64": null,
-                                            "asset": "navigation/direction_arrow_01"
-                                        }
-                                    }
-                                ]
-                            },
-                            {
-                                "type": "pictureGallery",
-                                "content": [
-                                    {
-                                        "base64": null,
-                                        "url": "http://42.159.6.37:3000/images/carpic6.png",
-                                        "asset": null
-                                     },
-                                     {
-                                        "base64": null,
-                                        "url": "http://42.159.6.37:3000/images/carpic6.png",
-                                        "asset": null
-                                     },
-                                     {
-                                        "base64": null,
-                                        "url": "http://42.159.6.37:3000/images/carpic6.png",
-                                        "asset": null
-                                     }
-                                ]
-                            }]
-                        }
-                    }]
+                    "entries": [myCarEntry, myCarEntry]
                 }]
             }
             // "events": [{
