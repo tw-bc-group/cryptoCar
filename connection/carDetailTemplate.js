a33be90815c86cdf049482ee00875deabed69a3e
RuipengWang <>
2018-09-14 08:05:29 +0800

    test -- ruipeng wang

--- connection/carDetailTemplate.js
+++ connection/carDetailTemplate.js
@@ -36,7 +36,7 @@ module.exports = {
     buildCarDetail: function (tokenId, bcm, navigatedMileage) {
         car = carMap[20000];
         console.log("**** detail json ****", JSON.stringify(car));
-        const detail = {
+        return {
             "location": "北京",
             "addPOIinfos": [ 
                 {
@@ -114,89 +114,6 @@ module.exports = {
         //         ]
         //     }
         // );
-        return {
-            "location": "北京",
-            "addPOIinfos": [
-                {
-                    "type": "subheader",
-                    "text": "DETAILS"
-                },
-                {
-                    "type": "row",
-                    "entries": [
-                        {
-                            "type": "column",
-                            "width": 100,
-                            "entries": [
-                                {
-                                    "type": "row",
-                                    "entries": [
-                                        {
-                                            "type": "image",
-                                            "picture": {
-                                                "base64": ""
-                                            },
-                                            "horizontalAlignment": "right",
-                                            "width": 30
-                                        },
-                                        {
-                                            "type": "text",
-                                            "width": 70,
-                                            "verticalAlignment": "center",
-                                            "horizontalAlignment": "left",
-                                            "font": "f12",
-                                            "color": "color_c07a",
-                                            "text": "(1611), 23333/Person"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "type": "row",
-                                    "entries": [
-                                        {
-                                            "type": "image",
-                                            "picture": {
-                                                "base64": ""
-                                            },
-                                            "horizontalAlignment": "center",
-                                            "width": 10
-                                        },
-                                        {}
-                                    ]
-                                },
-                                {
-                                    "type": "row",
-                                    "width": 90,
-                                    "verticalAlignment": "center",
-                                    "horizontalAlignment": "left",
-                                    "font": "f12",
-                                    "color": "color_c07a",
-                                    "text": "周一至周日\n00:00-21:00",
-                                    "entries": [
-                                        {
-                                            "type": "image",
-                                            "picture": {
-                                                "base64": ""
-                                            },
-                                            "horizontalAlignment": "center",
-                                            "width": 10
-                                        },
-                                        {
-                                            "type": "text",
-                                            "width": 90,
-                                            "verticalAlignment": "center",
-                                            "horizontalAlignment": "left",
-                                            "font": "f12",
-                                            "color": "color_c07a",
-                                            "text": "LBS\nLBS"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
+        // return 
     }
 };
